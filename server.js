7f0de1e42715d2c8799682318955cc06d6c15e22
Edglatus <tabrinath@gmail.com>
2022-10-25 00:43:59 -0300

    Moved db.json; removed unnecessary files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const cors = require('cors')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('tmp/db.json')
 const defaultMiddlewares = jsonServer.defaults()
 
 server.use(cors())
